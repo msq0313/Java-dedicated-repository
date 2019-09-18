cbe4ffffc321eeee04f7a5f3777283d472fa8db1
msq0313 <513299488@qq.com>
2019-09-18 20:49:21 +0800

    SARS

---
+++ SARS/SARS0912/移除元素.java
@@ -0,0 +1,19 @@
+package SARS.SARS0912;
+
+public class 移除元素 {
+    public static void main(String[] args) {
+        int[] nums = {3,2,2,3};
+        System.out.println(removeElement(nums, 3));
+    }
+
+    private static int removeElement(int[] nums, int val) {
+        int cnt = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[cnt] = nums[i];
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
